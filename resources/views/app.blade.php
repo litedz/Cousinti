485a68c3fc4100fa7f3f9ce42081825e2cfd6b0f
litedz <maamarjoker@gmail.com>
2023-03-31 00:13:38 +0100

    update name app

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="icon" type="image/x-icon" href="{{asset ('images/logo.png')}}">
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Cousinti') }}</title>
     
     @include('scripts.scripts_css')
 
