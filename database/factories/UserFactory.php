85d470c2ffe9c375c6cfd0ce5898c3be181ddb0c
litedz <maamarjoker@gmail.com>
2023-04-01 04:43:15 +0100

    fixed ranked problm

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,6 +25,7 @@ class UserFactory extends Factory
             'phone' => $this->faker->phoneNumber(),
             'email' => $this->faker->unique()->safeEmail(),
             'profile_id' => Profile::factory(),
+            'rank_id' => 3,
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => session()->token(),
